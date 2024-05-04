c2ac0d5b284de3186dbc0e65a3801340955ce7b2
Raphael Varieras <raphael@zello.com>
2024-05-04 07:39:57 -0500

    Flag for message

--- models/tmp/stg_twilio__message_tmp.sql
+++ models/tmp/stg_twilio__message_tmp.sql
@@ -1,2 +1,5 @@
+--To disable this model, set the using_twilio_message variable within your dbt_project.yml file to False.
+{{ config(enabled=var('using_twilio_message', True)) }}
+
 select * 
 from {{ var('message') }}
