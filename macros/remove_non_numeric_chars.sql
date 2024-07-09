0cb279c551e1b158e52a2868d91fb3261d98b53d
Renee Li <renee.li@fivetran.com>
2024-07-09 11:37:43 -0400

    first cast column as string

--- macros/remove_non_numeric_chars.sql
+++ macros/remove_non_numeric_chars.sql
@@ -5,18 +5,18 @@
 
 
 {% macro default__remove_non_numeric_chars(column) %}
-    REGEXP_REPLACE({{ column }}, '[^0-9.-]', '')
+    REGEXP_REPLACE(cast ({{ column }} as {{ dbt.type_string() }}), '[^0-9.-]', '')
 {% endmacro %}
 
 
 {% macro bigquery__remove_non_numeric_chars(column) %}
 
-    REGEXP_REPLACE({{ column }}, r'[^0-9.-]', '')
+    REGEXP_REPLACE(cast ({{ column }} as {{ dbt.type_string() }}), r'[^0-9.-]', '')
 
 {% endmacro %}
 
 {% macro postgres__remove_non_numeric_chars(column) %}
 
-    REGEXP_REPLACE({{ column }}, '[^0-9.-]', '', 'g')
+    REGEXP_REPLACE(cast ({{ column }} as {{ dbt.type_string() }}), '[^0-9.-]', '', 'g')
 
 {% endmacro %}
